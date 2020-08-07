1920c01fef8ea053e03b864e931427954a603f76
varsket1s <rytis6161@gmail.com>
2020-08-07 14:54:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+// Start session
+session_start(); 
+
+// Username and password
+$ID = "admin";
+$pass = "123456";
+
+if (isset($_POST["ID"]) && isset($_POST["pass"])) { 
+
+    if ($_POST["ID"] === $anvandarID && $_POST["pass"] === $pass) { 
+    /
+    $_SESSION["inloggedin"] = true; 
+
+    header("Location: safe_site.php"); 
+    exit; 
+    } 
+        // Wrong login - message
+        else {$wrong = "Bad ID and password, the system could not log you in";} 
+}
+
 error_reporting( error_reporting() & ~E_NOTICE );
 $allow_delete = true; 
 $allow_upload = true; 
